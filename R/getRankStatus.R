833f02c278ba97d124606845b4e2663687c03539
Jason <jmitchell@west-inc.com>
2016-05-25 12:30:56 -0600

    Change read.csv to not read in strings as factors.

--- R/getRankStatus.R
+++ R/getRankStatus.R
@@ -2,7 +2,7 @@ getRankStatus <- function(tblDir="//lar-file-srv/Data/BTPD_2016/Analysis/Databas
   
   # tblDir <- "//lar-file-srv/Data/BTPD_2016/Analysis/Database/"
 
-  ranks <- read.csv(paste0(tblDir,"tblSampling.csv"))
+  ranks <- read.csv(paste0(tblDir,"tblSampling.csv"),stringsAsFactors=FALSE)
   
   ranks
   
